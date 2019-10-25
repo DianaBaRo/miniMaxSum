10e64df5a42b9b5e4bf51d1496df00443e1d0556
Diana Carolina Badas <33879699+DianaBaRo@users.noreply.github.com>
2019-10-25 13:51:05 +0100

    Create solution

---
+++ solution.js
@@ -0,0 +1,20 @@
+function miniMaxSum(arr) {
+  let minValue, maxValue, minIndex, maxIndex, minResult = 0, maxResult = 0;
+  
+  minValue = Math.min(arr);
+  maxValue = Math.max(arr);
+  minIndex = arr.indexOf(minValue);
+  maxIndex = arr.indexOf(maxValue);
+  
+  
+  for (let i = 0; i < arr.length; i++) {
+    if ( minIndex !== i) {
+      maxResult += arr[i];
+    }
+    if ( maxIndex !== i ) {
+      minResult += arr[i];
+    }
+    console.log( minResult, maxResult );
+  }
+
+}
